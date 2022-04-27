c21b5ddd07806e995a0fa7bbd485818f92176eec
edaraanand <anand.edara@gmail.com>
2022-04-27 11:32:43 -0500

    First commit

---
+++ src/main/java/io/javabrains/ipldashboard/controller/TeamController.java
@@ -0,0 +1,30 @@
+package io.javabrains.ipldashboard.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.javabrains.ipldashboard.model.Team;
+import io.javabrains.ipldashboard.repository.MatchRepository;
+import io.javabrains.ipldashboard.repository.TeamRepository;
+
+@RestController
+public class TeamController {
+
+	private TeamRepository teamRepository;
+
+	private MatchRepository matchRepository;
+
+	public TeamController(TeamRepository teamRepository, MatchRepository matchRepository) {
+		this.teamRepository = teamRepository;
+		this.matchRepository = matchRepository;
+	}
+
+	@GetMapping("/team/{teamName}")
+	public Team getTeam(@PathVariable String teamName) {
+		Team team = this.teamRepository.findByTeamName(teamName);
+		team.setMatches(this.matchRepository.findTop4ByTeam1OrTeam2OrderByDateDesc(teamName, teamName));
+//		team.setMatches(this.matchRepository.findLatestMatchesByTeam(teamName, 4));
+		return team;
+	}
+}
